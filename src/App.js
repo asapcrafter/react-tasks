76d8af62703ee5de4cfc35c68401f0c24ff9f164
Steve <asapcrafter@gmail.com>
2021-08-23 22:20:34 -0700

    restarted the project

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      task: { text: ''},
+      tasks: [],
+    };
+  }
+
+  render() {
+    const { task, tasks} = this.state;
+
+    return (
+      <div>
+        <form>
+          <label htmlFor='taskInput'>Enter task</label>
+          <input type='text' id='taskInput'/>
+          <button type='submit'>
+            Add Task
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default App;
